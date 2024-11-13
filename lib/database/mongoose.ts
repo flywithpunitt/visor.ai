bf1f43bef5a5e9c57deff4da7c35043f2c0ddc27
flywithpunitt <punitkumar9168@gmail.com>
2024-11-13 16:17:43 +0530

    Fix ESLint errors and update types

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
@@ -7,37 +8,26 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-// Define a type for the global object with mongoose property
-interface CustomGlobal extends Global {
-  mongoose?: MongooseConnection;
-}
+let cached: MongooseConnection = (global as any).mongoose
 
-// Use proper typing for the global object
-declare const global: CustomGlobal;
-
-let cached: MongooseConnection = global.mongoose ?? {
-  conn: null,
-  promise: null
-};
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null
-  };
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, 
+    promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if(cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
       dbName: 'imaginify', 
       bufferCommands: false 
-    });
+    })
 
   cached.conn = await cached.promise;
 
